d48736b6c2640181f43571f1531eee28f96fd1b6
KevinIRIS <kz899@nyu.edu>
2017-03-11 22:56:28 -0500

    Finish

--- TweetMap/settings.py
+++ TweetMap/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p!5l!$gwrizxxyarn(xe&m@rc!j-)7a^bu0yvp$pze=3h9k67+'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -119,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+
+ALLOWED_HOSTS = ['custom-env-1.pmspppavfq.us-east-1.elasticbeanstalk.com']
